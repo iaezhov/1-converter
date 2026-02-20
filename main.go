e31ff856f15b683fe529745e63157bc36b9ac625
Ivan Ezhov <ivan.ezhov.94@gmail.com>
2026-02-20 15:46:45 +0300

    ДЗ 2

--- main.go
+++ main.go
@@ -3,9 +3,26 @@ package main
 import "fmt"
 
 func main() {
+	EURCount := getUserInput()
+	result := convertCurrency(EURCount, "EUR", "RUB")
+	outputResult(result)
+}
+
+func getUserInput() float64 {
+	var count float64
+	fmt.Scan(&count)
+	return count
+}
+
+func convertCurrency(count float64, currencyFrom string, currencyTo string) float64 {
+	fmt.Println("Изначальная валюта:", currencyFrom)
+	fmt.Println("Конвертация в валюту:", currencyTo)
 	const USDToEUR = 0.92
 	const USDToRUB = 76.64
 	const EURToRUB = USDToRUB / USDToEUR
-	var EURCount float64 = 100
-	fmt.Println(EURCount * EURToRUB)
+	return count * EURToRUB
+}
+
+func outputResult(value float64) {
+	fmt.Printf("Результат: %.0f", value)
 }
